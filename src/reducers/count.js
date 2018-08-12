1d2ed1b6a436d772ac376b9127f0d053944fee8f
yukinaka <yuki.n.0918@gmail.com>
2018-08-12 17:08:40 +0900

    fix

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -4,9 +4,9 @@ const initialState = { value: 0 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case 'INCREMENT':
       return { value: state.value +1 }
-    case DECREMENT:
+    case 'DECREMENT':
       return { value: state.value -1 }
     default:
       return state
